217a0495301eebf0f99b813397b0f84f0fd8ac6e
Bagas Ilham <rabbanilham@gmail.com>
2022-05-13 23:22:04 +0700

    Initial Commit

---
+++ Dummygram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dummygram
+//
+//  Created by Bagas Ilham on 13/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
